dad236c6c90cebb589321a854ea08221b6b07a9e
AdanSoria <joadsoriach@ittepic.edu.mx>
2025-11-20 11:57:51 -0700

    p31

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,2 +1,2 @@
 // Frontend/config.js
-export const API_BASE_URL = ' https://pseudodemocratic-darin-catalytically.ngrok-free.dev';
+export const API_BASE_URL = 'https://pseudodemocratic-darin-catalytically.ngrok-free.dev/api';
